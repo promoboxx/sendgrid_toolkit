8cf0d05488a12be410233dcbaa984f561ae5f32c
Joseph Ware <joseph@promoboxx.com>
2025-12-08 10:54:04 -0500

    DEV-11922 Add V3 /mail/send endpoint (#6)

---
+++ lib/sendgrid_toolkit/v3/mail.rb
@@ -0,0 +1,20 @@
+require 'json'
+
+module SendgridToolkit
+  module V3
+    class Mail < SendgridToolkit::V3::AbstractSendgridClient
+      def send_mail(options = {})
+        response = api_post('mail/send', convert_params(options))
+        raise(SendEmailError, "SendMail API refused to send email: #{response["errors"].inspect}") if response["message"] == "error"
+        response
+      end
+
+      private
+
+      def convert_params(options)
+        options["x-smtpapi"] = options["x-smtpapi"].to_json if options.has_key?("x-smtpapi")
+        options
+      end
+    end
+  end
+end
